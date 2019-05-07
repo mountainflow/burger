0a437f25429fa76b9904500ce54c13b836b02b9e
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-05-07 15:02:12 -0600

    basic structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Blue Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Works', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Jane', false);
+
+SELECT * FROM burgers;
